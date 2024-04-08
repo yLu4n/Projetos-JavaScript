6253e0d3db003ba078a342fe420bde483308b62c
Luan Victor <107703846+yLu4n@users.noreply.github.com>
2024-04-08 12:01:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-let caixaEntrada = document.getElementById('caixaEntrada')
-let botoes = document.getElementById('botao')
 
-let string = ''
-
-botoes.forEach(element => {
-    element.addEventeListener('click', (b)=> {
-        if(b.target.innerText == '='){
-            string = String(eval(string))
-            caixaEntrada.value = string;
-        }
-        else if(b.target.innerText == 'AC')
-        else{
-            string += b.target.innerText
-            caixaEntrada.value = string
-        }
-    })
-});
